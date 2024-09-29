e92cfb9f09b8d882e1fe93bdda18b4d2362a5bf9
Riaan Roos <rroos64@gmail.com>
2024-09-29 08:04:23 +0200

    Revert "Attempt to change the spinner bg colour"
    This reverts commit ffc60552dcef5f5c31bd0abd0916adfb1c89ee3d.

--- src/components/buttons/SendButton.tsx
+++ src/components/buttons/SendButton.tsx
@@ -54,7 +54,7 @@ export const DeleteButton = (props: SendButtonProps) => {
 export const Spinner = (props: JSX.SvgSVGAttributes<SVGSVGElement>) => (
   <svg
     {...props}
-    class={'animate-spin -ml-1 mr-3 h-5 w-5 spinner-bg-custom' + props.class}
+    class={'animate-spin -ml-1 mr-3 h-5 w-5 ' + props.class}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
